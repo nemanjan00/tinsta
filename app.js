8143c815082671c6b704038f435477bbb3e66745
Nemanja Nedeljkovic <nemanjan00@gmail.com>
2018-04-14 19:55:27 +0200

    GUI mockup

--- app.js
+++ app.js
@@ -9,43 +9,45 @@ screen.title = "tinsta";
 
 // Create a box perfectly centered horizontally and vertically.
 const box = blessed.box({
-	top: "center",
-	left: "center",
-	width: "50%",
-	height: "50%",
+	style: {
+		focus: {
+			border: {
+				fg: "blue"
+			}
+		}
+	},
+	width: "20%",
+	height: "100%",
 	content: "Hello {bold}world{/bold}!",
 	tags: true,
 	border: {
 		type: "line"
-	},
-	style: {
-		fg: "white",
-		bg: "magenta",
-		border: {
-			fg: "#f0f0f0"
-		},
-		hover: {
-			bg: "green"
-		}
 	}
 });
 
 // Append our box to the screen.
 screen.append(box);
 
-// If our box is clicked, change the content.
-box.on("click", function() {
-	box.setContent("{center}Some different {red-fg}content{/red-fg}.{/center}");
-	screen.render();
+// Create a box perfectly centered horizontally and vertically.
+const box1 = blessed.box({
+	style: {
+		focus: {
+			border: {
+				fg: "blue"
+			}
+		}
+	},
+	left: "20%",
+	width: "80%",
+	height: "100%",
+	content: "Hello {bold}world{/bold}!",
+	tags: true,
+	border: {
+		type: "line"
+	}
 });
 
-// If box is focused, handle `enter`/`return` and give us some more content.
-box.key("enter", function() {
-	box.setContent("{right}Even different {black-fg}content{/black-fg}.{/right}\n");
-	box.setLine(1, "bar");
-	box.insertLine(1, "foo");
-	screen.render();
-});
+screen.append(box1);
 
 // Quit on Escape, q, or Control-C.
 screen.key(["escape", "q", "C-c"], function() {
