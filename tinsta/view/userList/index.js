3720a9f8dc6716e440d70d6f32727ce475502ef5
Nemanja Nedeljkovic <nemanjan00@gmail.com>
2018-04-14 20:52:50 +0200

    Mockup

--- tinsta/view/userList/index.js
+++ tinsta/view/userList/index.js
@@ -3,24 +3,35 @@ const config = require("../../config");
 
 const screen = config.get("screen");
 
-const box = blessed.box({
+const list = blessed.list({
 	style: {
 		focus: {
 			border: {
 				fg: "blue"
 			}
+		},
+		selected: {
+			fg: "blue"
 		}
 	},
+	mouse: true,
 	width: "20%",
 	height: "100%",
-	content: "Hello {bold}world{/bold}!",
 	tags: true,
 	border: {
 		type: "line"
 	}
 });
 
-screen.append(box);
+screen.append(list);
 
-module.exports = box;
+list.addItem("@instazavodnik");
+list.addItem("\t@nemanjan00");
+
+list.on("select", () => {
+});
+
+list.focus();
+
+module.exports = list;
 
