1cac3d1ecf91b3a2b62003af4a05a92d222b7660
Nemanja Nedeljkovic <nemanjan00@gmail.com>
2018-04-14 19:37:46 +0200

    Linting of example

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-var blessed = require('blessed');
+const blessed = require("blessed");
 
 // Create a screen object.
-var screen = blessed.screen({
+const screen = blessed.screen({
 	smartCSR: true
 });
 
-screen.title = 'my window title';
+screen.title = "tinsta";
 
 // Create a box perfectly centered horizontally and vertically.
-var box = blessed.box({
-	top: 'center',
-	left: 'center',
-	width: '50%',
-	height: '50%',
-	content: 'Hello {bold}world{/bold}!',
+const box = blessed.box({
+	top: "center",
+	left: "center",
+	width: "50%",
+	height: "50%",
+	content: "Hello {bold}world{/bold}!",
 	tags: true,
 	border: {
-		type: 'line'
+		type: "line"
 	},
 	style: {
-		fg: 'white',
-		bg: 'magenta',
+		fg: "white",
+		bg: "magenta",
 		border: {
-			fg: '#f0f0f0'
+			fg: "#f0f0f0"
 		},
 		hover: {
-			bg: 'green'
+			bg: "green"
 		}
 	}
 });
@@ -33,34 +33,22 @@ var box = blessed.box({
 // Append our box to the screen.
 screen.append(box);
 
-// Add a png icon to the box
-var icon = blessed.image({
-	parent: box,
-	top: 0,
-	left: 0,
-	type: 'overlay',
-	width: 'shrink',
-	height: 'shrink',
-	file: __dirname + '/my-program-icon.png',
-	search: false
-});
-
 // If our box is clicked, change the content.
-box.on('click', function(data) {
-	box.setContent('{center}Some different {red-fg}content{/red-fg}.{/center}');
+box.on("click", function() {
+	box.setContent("{center}Some different {red-fg}content{/red-fg}.{/center}");
 	screen.render();
 });
 
 // If box is focused, handle `enter`/`return` and give us some more content.
-box.key('enter', function(ch, key) {
-	box.setContent('{right}Even different {black-fg}content{/black-fg}.{/right}\n');
-	box.setLine(1, 'bar');
-	box.insertLine(1, 'foo');
+box.key("enter", function() {
+	box.setContent("{right}Even different {black-fg}content{/black-fg}.{/right}\n");
+	box.setLine(1, "bar");
+	box.insertLine(1, "foo");
 	screen.render();
 });
 
 // Quit on Escape, q, or Control-C.
-screen.key(['escape', 'q', 'C-c'], function(ch, key) {
+screen.key(["escape", "q", "C-c"], function() {
 	return process.exit(0);
 });
 
@@ -69,3 +57,4 @@ box.focus();
 
 // Render the screen.
 screen.render();
+
